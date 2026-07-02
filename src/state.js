c465546e8ce79336166123eaf7bf92e510c7877f
dianuro <dianuro@qq.com>
2026-07-03 02:10:17 +0800

    feat: frontend HTML structure and state management (task 8/11)

---
+++ src/state.js
@@ -0,0 +1,13 @@
+// 简单应用状态管理
+const state = {
+  currentFile: null,
+  currentHtml: null,
+  metadata: null,
+  fileTree: [],
+  searchResults: [],
+  searchIndex: 0,
+  sidebarVisible: false,
+  darkMode: false,
+};
+
+export default state;
